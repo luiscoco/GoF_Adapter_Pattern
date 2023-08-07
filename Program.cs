f750a50beabed886fa56e114a3c023bc967327c8
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-08-07 12:22:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Client code using the adapter
+        LegacyPrinter legacyPrinter = new LegacyPrinter();
+        PrinterAdapter printerAdapter = new PrinterAdapter(legacyPrinter);
+        Client client = new Client(printerAdapter);
+
+        client.Print("Hello, Adapter Pattern!");
+
+        // Output will be:
+        // Legacy Printer: Hello, Adapter Pattern!
+    }
+}
+
+// Existing class with an incompatible interface
+public class LegacyPrinter
+{
+    public void Print(string text)
+    {
+        Console.WriteLine($"Legacy Printer: {text}");
+    }
+}
+
+// New interface that clients expect
+public interface IPrinter
+{
+    void PrintText(string text);
+}
+
+// Adapter class that implements the new interface and adapts the LegacyPrinter
+public class PrinterAdapter : IPrinter
+{
+    private LegacyPrinter legacyPrinter;
+
+    public PrinterAdapter(LegacyPrinter legacyPrinter)
+    {
+        this.legacyPrinter = legacyPrinter;
+    }
+
+    public void PrintText(string text)
+    {
+        // The adapter calls the appropriate method of the LegacyPrinter
+        legacyPrinter.Print(text);
+    }
+}
+
+// Client code that expects the IPrinter interface
+public class Client
+{
+    private IPrinter printer;
+
+    public Client(IPrinter printer)
+    {
+        this.printer = printer;
+    }
+
+    public void Print(string text)
+    {
+        printer.PrintText(text);
+    }
+}
+
